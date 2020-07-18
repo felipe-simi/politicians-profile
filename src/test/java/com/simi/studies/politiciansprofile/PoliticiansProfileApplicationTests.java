a3f968888202294fd0b8b3284ee87699d283f0a6
Felipe Miranda <felip.simi@gmail.com>
2020-07-18 19:15:27 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/test/java/com/simi/studies/politiciansprofile/PoliticiansProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.studies.politiciansprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticiansProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
