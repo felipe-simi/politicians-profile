52f2459ef37824f73bcc52024e397fdf892e3704
Felipe Miranda <felip.simi@gmail.com>
2020-07-20 21:28:43 -0300

    Redes sociais: criacao da classe de dominio

---
+++ src/main/java/com/simi/studies/politiciansprofile/politician/command/domain/model/SocialNetwork.java
@@ -0,0 +1,11 @@
+package com.simi.studies.politiciansprofile.politician.command.domain.model;
+
+import lombok.Value;
+
+@Value
+public class SocialNetwork {
+
+  private String name;
+  private String link;
+
+}
