c157bab78c16195401a875201a1566554c355f62
Felipe Miranda <felip.simi@gmail.com>
2020-07-21 10:19:18 -0300

    Endereco: adicao de transformar string num tipo de endereco

--- src/main/java/com/simi/studies/politiciansprofile/politician/command/domain/model/AddressType.java
+++ src/main/java/com/simi/studies/politiciansprofile/politician/command/domain/model/AddressType.java
@@ -1,5 +1,7 @@
 package com.simi.studies.politiciansprofile.politician.command.domain.model;
 
+import java.util.Optional;
+
 public enum AddressType {
 
   RESIDENTIAL("residential"), COMMERCIAL("commercial");
@@ -8,6 +10,19 @@ public enum AddressType {
 
   AddressType(String type) {
     this.type = type;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public Optional<AddressType> getDocumentType(String value) {
+    for (AddressType addressType : AddressType.values()) {
+      if (addressType.type.equalsIgnoreCase(value)) {
+        return Optional.of(addressType);
+      }
+    }
+    return Optional.empty();
   }
 
 }
