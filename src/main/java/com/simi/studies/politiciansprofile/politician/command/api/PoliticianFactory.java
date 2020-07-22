1aaf70af609bc2edf6c95a4e9fa7f846c50461d1
Felipe Miranda <felip.simi@gmail.com>
2020-07-22 20:28:53 -0300

    Politico: fabrica de criacao de objetos politico

---
+++ src/main/java/com/simi/studies/politiciansprofile/politician/command/api/PoliticianFactory.java
@@ -0,0 +1,13 @@
+package com.simi.studies.politiciansprofile.politician.command.api;
+
+import com.simi.studies.politiciansprofile.politician.command.domain.model.Address;
+import com.simi.studies.politiciansprofile.politician.command.domain.model.DocumentId;
+import com.simi.studies.politiciansprofile.politician.command.domain.model.Politician;
+
+public interface PoliticianFactory {
+
+  Politician create(final DocumentId id);
+
+  Politician create(final DocumentId id, final Address address);
+
+}
