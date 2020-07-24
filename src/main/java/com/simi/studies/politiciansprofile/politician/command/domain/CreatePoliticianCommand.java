2e77893eda578326a2cd7d2ad00ca0fb3e2e2bc3
Felipe Miranda <felip.simi@gmail.com>
2020-07-23 22:03:46 -0300

    Comandos: refactoracao do comando de politico para validacao dos atributos e inclusao do endereco

--- src/main/java/com/simi/studies/politiciansprofile/politician/command/domain/CreatePoliticianCommand.java
+++ src/main/java/com/simi/studies/politiciansprofile/politician/command/domain/CreatePoliticianCommand.java
@@ -1,19 +1,32 @@
 package com.simi.studies.politiciansprofile.politician.command.domain;
 
-import com.simi.studies.politiciansprofile.politician.command.domain.model.Address;
-import com.simi.studies.politiciansprofile.politician.command.domain.model.DocumentType;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class CreatePoliticianCommand {
 
+  @NotNull
   private UUID id;
-  private DocumentType documentType;
-  private Address address;
+  @NotBlank
+  private String documentType;
+  @NotBlank
+  private String documentCode;
+  @Valid
+  private AddressCommand address;
 
+  public CreatePoliticianCommand(@NotNull UUID id,
+                                 @NotBlank String documentType,
+                                 @NotBlank String documentCode,
+                                 @Valid AddressCommand address) {
+    this.id = id;
+    this.documentType = documentType;
+    this.documentCode = documentCode;
+    this.address = address;
+  }
 }
