b5214c7fc72b0a771afbd15cfa2f18f996ed24fd
Felipe Miranda <felip.simi@gmail.com>
2020-09-15 01:14:03 -0300

    Testes: lendo do arquivo e convertendo em objetos

--- src/test/java/com/simi/studies/politiciansprofile/util/JsonArgumentsProvider.java
+++ src/test/java/com/simi/studies/politiciansprofile/util/JsonArgumentsProvider.java
@@ -1,5 +1,7 @@
 package com.simi.studies.politiciansprofile.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
@@ -8,24 +10,29 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class JsonArgumentsProvider implements ArgumentsProvider, AnnotationConsumer<JsonSource> {
 
+  private final ObjectMapper objectMapper = new ObjectMapper();
   private final ClassLoader classLoader = this.getClass().getClassLoader();
   private Class<?> argumentsType;
   private String[] resources;
 
   @Override
-  public Stream<? extends Arguments> provideArguments(final ExtensionContext context) {
-    return null;
-  }
-
-  @Override
   public void accept(final JsonSource jsonSource) {
     this.argumentsType = jsonSource.argumentsType();
     this.resources = jsonSource.resources();
+  }
+
+  @Override
+  public Stream<? extends Arguments> provideArguments(final ExtensionContext context) {
+    return Stream.of(resources)
+        .map(this::fromPathAsString)
+        .map(convertToObject())
+        .map(object -> Arguments.of(object));
   }
 
   private String fromPathAsString(final String fileName) {
@@ -40,6 +47,17 @@ public class JsonArgumentsProvider implements ArgumentsProvider, AnnotationConsu
       ex.printStackTrace();
       throw new RuntimeException(ex);
     }
+  }
+
+  private Function<String, ?> convertToObject() {
+    return body -> {
+      try {
+        return objectMapper.readValue(body, argumentsType);
+      } catch (JsonProcessingException ex) {
+        ex.printStackTrace();
+        throw new RuntimeException(ex);
+      }
+    };
   }
 
 }
---
+++ src/test/java/com/simi/studies/politiciansprofile/util/JsonSourceTest.java
@@ -0,0 +1,36 @@
+package com.simi.studies.politiciansprofile.util;
+
+import com.simi.studies.politiciansprofile.politician.command.client.CreatePoliceRequestBody;
+import org.junit.jupiter.params.ParameterizedTest;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JsonSourceTest {
+
+  @ParameterizedTest
+  @JsonSource(resources = "politicians/valid_politician.json")
+  void givenNoArgumentsType_WhenParameterizedTest_ThenShouldFillAsMap(final Map politician) {
+    final var expectedPolitician = Map.of("documentCode", "12345678",
+        "documentType", "RG",
+        "name", "Valid Name");
+    assertAll("Can read politician from JSON file",
+        () -> assertEquals(expectedPolitician.get("documentCode"), politician.get("documentCode")),
+        () -> assertEquals(expectedPolitician.get("documentType"), politician.get("documentType")),
+        () -> assertEquals(expectedPolitician.get("name"), politician.get("name")));
+  }
+
+  @ParameterizedTest
+  @JsonSource(argumentsType = CreatePoliceRequestBody.class, resources = "politicians/valid_politician.json")
+  void givenArgumentsType_WhenParameterizedTest_ThenShouldFillAsObject(
+      final CreatePoliceRequestBody requestBody) {
+    final var expectedPolitician = Map.of("documentCode", "12345678",
+        "documentType", "RG",
+        "name", "Valid Name");
+    assertAll("Can read politician from JSON file",
+        () -> assertEquals(expectedPolitician.get("documentCode"), requestBody.getDocumentCode()),
+        () -> assertEquals(expectedPolitician.get("documentType"), requestBody.getDocumentType()),
+        () -> assertEquals(expectedPolitician.get("name"), requestBody.getName()));
+  }
+
+}
