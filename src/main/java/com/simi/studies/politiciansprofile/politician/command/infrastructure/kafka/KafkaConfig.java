2768b365f63ae9302e9595c4fba43a993fc80ef2
Felipe Miranda <felip.simi@gmail.com>
2020-10-02 19:27:34 -0300

    Eventos: habilitando o Kafka no Spring Boot

---
+++ src/main/java/com/simi/studies/politiciansprofile/politician/command/infrastructure/kafka/KafkaConfig.java
@@ -0,0 +1,9 @@
+package com.simi.studies.politiciansprofile.politician.command.infrastructure.kafka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+}
